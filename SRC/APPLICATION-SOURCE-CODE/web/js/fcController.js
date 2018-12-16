33256ef7b1540d2ae1f78c0162f8b542de4cce4e
yuval weiss <yuval15111@gmail.com>
2018-12-16 21:28:38 +0200

    Added google maps api

--- SRC/APPLICATION-SOURCE-CODE/web/js/fcController.js
+++ SRC/APPLICATION-SOURCE-CODE/web/js/fcController.js
@@ -1,13 +1,15 @@
 
 let app = angular.module('myApp', []);
-app.controller('mainController', function($scope, require) { //$resource
+app.controller('mainController', ['$scope', function($scope, require, $sce) { //$resource
 
     // let router = require('/:loc/:lag/location', {loc : '@loc', lag : '@lag'});
-    $scope.listOfShit = ['hi', 'bye', 'yossi'];
-    $scope.nameOfShit = 'yossi';
+
     // router.$get({loc : '128', lag : '189'}, function(res) {
     //
     // });
+    //$sce.trustAsUrl("https://www.google.com/maps/embed/v1/place?key=AIzaSyDVi-8TbF6uKXPHynmXSztCauLR7WjKI74&q=American+museum+of+Natural+History+New+York+City");
 
+    $scope.mapsQuery = $sce.trustAsUrl("https://www.google.com/maps/embed/v1/place?key=AIzaSyDVi-8TbF6uKXPHynmXSztCauLR7WjKI74&q=American+museum+of+Natural+History+New+York+City");
+    //$scope.mapsQuery += "American+museum+of+Natural+History+New+York+City" // the answer from the server
 
-});
+}]);
