01fe0234b4f062b668ebfe3aefdf0b4196615341
yuval weiss <yuval15111@gmail.com>
2019-01-02 19:55:34 +0200

    many web changes

--- SRC/APPLICATION-SOURCE-CODE/web/js/fcController.js
+++ SRC/APPLICATION-SOURCE-CODE/web/js/fcController.js
@@ -1,35 +1,133 @@
 
-let app = angular.module('myApp', []);
+let app = angular.module('myApp', ["angucomplete-alt"]);
 app.controller('mainController', ['$scope', function($scope) { //$resource /*, $sce, require  '$sce','require'*/
 
     // let router = require('/:loc/:lag/location', {loc : '@loc', lag : '@lag'});
+    //let autoCompleteRouter = require ('/:text/:type/autoComplete', {text : '@text', type : '@type'});
+    //let ingredientRouter = require ('/:ingredient/:priceLevel/:delivery/autoComplete', {text : '@text', type : '@type'});
+
 
     // router.$get({loc : '128', lag : '189'}, function(res) {
     //
     // });
-    //$sce.trustAsUrl("https://www.google.com/maps/embed/v1/place?key=AIzaSyDVi-8TbF6uKXPHynmXSztCauLR7WjKI74&q=American+museum+of+Natural+History+New+York+City");
 
-    //$scope.mapsQuery = $sce.trustAsUrl("https://www.google.com/maps/embed/v1/place?key=AIzaSyDVi-8TbF6uKXPHynmXSztCauLR7WjKI74&q=American+museum+of+Natural+History+New+York+City");
-    //$scope.mapsQuery += "American+museum+of+Natural+History+New+York+City" // the answer from the server
+    $scope.currentLocation = 0;
 
+    $scope.searchType = 0;
+    $scope.autoCompleteRes = {};
+    $scope.ingredPriceLevel = 0;
+    $scope.ingredDelivery = 0;
 
-
-    //document.getElementById("googleLocation").src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyDVi-8TbF6uKXPHynmXSztCauLR7WjKI74&q=central+park+New+York+City";
-    $scope.favor = "hello";
-    $scope.favorFunc = function(){
-        $scope.favor = document.getElementById("favorC").value; // Just for debugging
-        let result = document.getElementById("favorC").value;
-        // Pass result to the server
+    // Determine the view for each query
+    $scope.decision = function (dec) {
+        $scope.searchType = dec;
     };
-    $scope.reply = {1 : "Italian", 2 : "french", 3 : "bulgarian", 4 : "American"};
 
-    // Ingredients function
-    $scope.ingredFunc = function() {
-        let i;
-        $scope.ingred = [];
-        for(i = 1; i <= 4; i++) {
-            $scope.ingred.push(document.getElementById("ingred"+i).value);
-            // Pass to server ingred array
+
+
+    // Auto complete according to the search type and the input text
+    $scope.autoComplete = function(/*text*/) {
+      //   autoCompleteRouter.$get({text : text, this.searchStr : $scope.searchType}, function(res) {
+      //
+      // })\
+
+
+        $scope.autoCompleteRes = [
+            {entry_name : 'gbc'},
+            {entry_name : 'jhg'},
+            {entry_name : 'acc'},
+            {entry_name : 'ahc'},
+            {entry_name : 'gbc'},
+            {entry_name : 'lui'},
+            {entry_name : '123'},
+            {entry_name : 'jhgr'},
+            ];
+
+        if ($scope.searchType == 0) {
+            $scope.currIngredient = this.searchStr;
+        }
+        else if ($scope.searchType == 1) {
+            $scope.currResturant = this.searchStr;
+        }
+        else if ($scope.searchType == 2) {
+            $scope.currUniqueIngred = this.searchStr;
+        }
+    };
+
+    $scope.submitIngredient = function() {
+        // ingredientRouter.$get({ingredient :$scope.currIngredient}, function(res) {
+        //
+        // });
+
+        // send to ingredient router
+        let a = $scope.currIngredient;
+        let b = $scope.ingredPriceLevel;
+        let c = $scope.ingredDelivery;
+        let d = $scope.currentLocation;
+
+
+        //example for result
+        $scope.restFromIngred = [{restaurant_name : 'ret1', cuisine : "Italian" ,agg_review : 3.2, lat : 42, lng : 39, price_category : 3, featured_photo_url : "url"}, {restaurant_name : 'ret1', agg_review : 3.2, lat : 142, lng : 152, price_category : 3, featured_photo_url : "url"}, {restaurant_name : 'ret1', agg_review : 3.2, lat : 142, lng : 152, price_category : 3, featured_photo_url : "url"}];
+    };
+
+
+    $scope.submitDiscoverNewCuisine = function () {
+      // discoverRouter.$get({}, function(res){});
+        let a = $scope.currResturant;
+        $scope.discoverNewCuisine = [{cuisine_name : 'Italian'}, {cuisine_name : 'Italian'}, {cuisine_name : 'Italian'}];
+    };
+
+
+    $scope.submitUniqueIngredients = function() {
+        // uniqueIngredRouter.$get({}, function(res){});
+        let a = $scope.currUniqueIngred;
+
+        $scope.uniqueIngredintsResult = [{ingredient : "apple"}, {ingredient : "banana"}];
+    };
+
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoicmFtZmxvIiwiYSI6ImNqcWZjNmFuajUzMHo0YW1zeTJ5ZDFrMTcifQ.-6GAbzLXvYr7ftYbYeKMUg';
+    let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [-73.996155, 40.732013],
+        zoom: 10
+    });
+
+
+    $scope.locateRestInMap = function (rest) {
+        let geojson = {
+            type: 'FeatureCollection',
+            features: [{
+                type: 'Feature',
+                geometry: {
+                    type: 'Point',
+                    coordinates: [rest.lng, rest.lat]
+                },
+                properties: {
+                    title: rest.cuisine,
+                    description: rest.restaurant_name
+                }
+            }]
         };
+
+        // add markers to map
+        geojson.features.forEach(function(marker) {
+
+            // create a HTML element for each feature
+            let el = document.createElement('div');
+            el.className = 'marker';
+
+            // make a marker for each feature and add to the map
+            new mapboxgl.Marker(el)
+                .setLngLat(marker.geometry.coordinates)
+                .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+                    .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+                .addTo(map);
+        });
+        map.flyTo({center: [rest.lng, rest.lat] ,zoom : 14});
     };
+
+
+
 }]);
