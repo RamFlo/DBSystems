0c259170c3b12db7edbc6da9dfeab9ae9098e881
michaeluniversity <michaelk@mail.tau.ac.il>
2019-01-19 22:54:44 +0200

    [Backend][Server][Logger] Rename loggers and small changes

--- SRC/APPLICATION-SOURCE-CODE/log.py
+++ SRC/APPLICATION-SOURCE-CODE/log.py
@@ -1,11 +1,11 @@
 import logging
 import logging.config
 
+
 class Logger:
-  def __init__(self, t="valid"):
-    logging.config.fileConfig('logging.conf')
-    if (t == "error"):
-        self.logger = logging.getLogger("michaelsProjectError")
-    else:
-        self.logger = logging.getLogger("michaelsProject")
-    
+    def __init__(self, t="valid"):
+        logging.config.fileConfig('logging.conf')
+        if t == "error":
+            self.logger = logging.getLogger("ErrorLog")
+        else:
+            self.logger = logging.getLogger("ServerLog")
