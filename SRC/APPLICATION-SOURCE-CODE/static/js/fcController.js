287c5211ed1f82f4ac86ed0dcd05a58dc6526d45
Ram S <ramsh92@gmail.com>
2019-01-21 19:15:12 +0200

    cuisine page finishes 3

--- SRC/APPLICATION-SOURCE-CODE/static/js/fcController.js
+++ SRC/APPLICATION-SOURCE-CODE/static/js/fcController.js
@@ -214,7 +214,7 @@ app.controller('mainController', ['$scope','$rootScope','$timeout','DTOptionsBui
 
     $scope.disableAutoOrderTableOptions = DTOptionsBuilder.newOptions().withOption('order',[]);
 
-    $scope.cuisineUniqueTableOptions = DTOptionsBuilder.newOptions().withOption('order',[2,'desc']);
+    $scope.cuisineUniqueTableOptions = DTOptionsBuilder.newOptions().withOption('order',[[2,'desc']]);
 
     populateCuisineList();
 
